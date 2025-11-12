078361e206da756bcf30909a31ffbd4d81b9e741
Swati Khedekar <khedekarswati75@gmail.com>
2025-11-12 16:09:18 +0530

    Add files via upload

--- case study1_danny diner/1_danys_dinner.sql
+++ case study1_danny diner/1_danys_dinner.sql
@@ -1,4 +1,4 @@
-#  Danny dinner 1 sql challenge
+###  Danny dinner 1 sql challenge
 create database danny_dinner;
 use danny_dinner;
 
@@ -47,94 +47,127 @@ INSERT INTO members
 VALUES
   ('A', '2021-01-07'),
   ('B', '2021-01-09');
-  
-drop table members;
+
 select * from sales;
 select * from menu;
 select * from members;
 
-
-  
   /* --------------------
    Case Study Questions
    --------------------*/
 -- 1. What is the total amount each customer spent at the restaurant?
 select s.customer_id as Customer,sum(m.price) as Total_amount_spent
-from sales s JOIN menu m 
-on m.product_id = s.product_id
+from 
+	sales s JOIN menu m on m.product_id = s.product_id
 group by 1;
 
 -- 2. How many days has each customer visited the restaurant?
-select customer_id as Customer, count(distinct order_date) as day_visited from sales group by 1 ;
+select * from sales;
+select customer_id as Customer, count(distinct order_date) as day_visited 
+from 
+	sales 
+group by 1 ;
+
+SET sql_mode = (SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));
 
 -- 3. What was the first item from the menu purchased by each customer?
 select s.customer_id as Customer, m.product_name as first_item_purchsed 
-from sales s join menu m
-on s.product_id = m.product_id group by 1;
+from 
+	sales s join menu m
+	on s.product_id = m.product_id 
+group by Customer ;
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
-select m.product_name, count(*) as most_purchased_item from sales s 
-join menu m on s.product_id = m.product_id  
+select * from sales;
+select * from menu;
+select m.product_name, count(*) as most_purchased_item 
+from 
+	sales s join menu m on s.product_id = m.product_id  
 group by 1
 order by 2 desc limit 1;
 
 # for each item how many time purchased
 select m.product_name, count(*) as most_purchased_item
-from sales s join menu m 
-on s.product_id = m.product_id  group by 1;
+from 
+	sales s join menu m 
+	on s.product_id = m.product_id  
+group by 1;
 
 -- 5. Which item was the most popular for each customer?
 select s.customer_id as customer, m.product_name as most_popular_item,count(*) as total_item
-from sales s join menu m 
-on s.product_id = m.product_id group by 1;
+from 
+	sales s join menu m 
+	on s.product_id = m.product_id 
+group by 1
+order by 3 desc;
 
 -- 6. Which item was purchased first by the customer after they became a member?
-select s.customer_id as customer,m.product_name as item_purchased_first,m1.join_date,s.order_date from sales s
-join menu m on s.product_id = m.product_id 
-join members m1 on s.customer_id = m1.customer_id 
-where s.order_date = m1.join_date or s.order_date > m1.join_date
+select s.customer_id as customer,
+	m.product_name as item_purchased_first,
+	m1.join_date,
+    s.order_date 
+from 
+	sales s join menu m 
+	on s.product_id = m.product_id 
+	join members m1 
+    on s.customer_id = m1.customer_id 
+where 
+	(s.order_date = m1.join_date) or (s.order_date > m1.join_date)
 group by 1
 order by 2 ;
 
 -- 7. Which item was purchased just before the customer became a member?
-select s.customer_id as customer,m.product_name as item_purchased_before_member,m1.join_date,
-s.order_date from sales s
-join menu m on s.product_id = m.product_id 
-join members m1 on s.customer_id = m1.customer_id 
-where s.order_date = m1.join_date or s.order_date < m1.join_date
+select s.customer_id as customer,
+	m.product_name as item_purchased_before_member,
+	m1.join_date,
+	s.order_date 
+from 
+	sales s join menu m on s.product_id = m.product_id 
+	join members m1 on s.customer_id = m1.customer_id 
+where 
+	(s.order_date = m1.join_date) or (s.order_date < m1.join_date)
 group by 1
 order by 1 ;
 
 -- 8. What is the total items and amount spent for each member before they became a member?
-select s.customer_id as customer,count(*) as total_item,
-sum(m.price) as Total_amount_spent,m1.join_date,s.order_date from sales s
-join menu m on s.product_id = m.product_id 
-join members m1 on s.customer_id = m1.customer_id 
-where s.order_date = m1.join_date or s.order_date > m1.join_date
+select s.customer_id as customer,
+	count(*) as total_item,
+	sum(m.price) as Total_amount_spent,
+	m1.join_date,
+	s.order_date 
+from 
+	sales s join menu m on s.product_id = m.product_id 
+	join members m1 on s.customer_id = m1.customer_id 
+where 
+	(s.order_date = m1.join_date) or (s.order_date < m1.join_date)
 group by 1
 order by 1 ;
 
 /* 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier 
  how many points would each customer have ?*/
-select sub.customer,sum(points) as Total_points
+select sub.customer,
+	sum(points) as Total_points
 from
-(select s.customer_id as customer,
-case 
-when m.product_name ='sushi' then 2*10*m.price
-else 10*m.price 
-end as points
-from sales s left join menu m on s.product_id = m.product_id) sub
+	(select s.customer_id as customer,
+	case 
+		when m.product_name ='sushi' then 2*10*m.price
+	else 10*m.price 
+	end as points
+	from 
+		sales s left join menu m on s.product_id = m.product_id) sub
 group by 1;
 
 /* 10. In the first week after a customer joins the program (including their join date) they earn
 2x points on all items, not just sushi - how many points do customer A and B have at the end of January? */
  select s.customer_id ,
- sum(case 
+	sum(
+    case 
 	when s.order_date between m1.join_date and date_add(m1.join_date,interval 6 day)
-	then m.price*2*10 when m.product_name = 'sushi' then m.price*2*10
+		then m.price*2*10 when m.product_name = 'sushi' then m.price*2*10
 	else m.price*10 
 	end) as total_points from sales s 
- join menu m on m.product_id = s.product_id
- join members m1 on m1.customer_id =s.customer_id
+	join menu m on m.product_id = s.product_id
+	join members m1 on m1.customer_id =s.customer_id
  where date_format(s.order_date,'%Y-%m-01')='2021-01-01'
- group by 1 order by 1;
+ group by 1 
+ order by 1;
